996b3403a70dc322aaec0499cf01c1b07da30e5f
Dane Stuckey <dane@dane.io>
2018-05-13 18:02:16 -0700

    Minor CI tweak.

--- ci/packet_install_script.sh
+++ ci/packet_install_script.sh
@@ -30,6 +30,9 @@ wget https://releases.hashicorp.com/packer/1.2.3/packer_1.2.3_linux_amd64.zip
 unzip packer_1.2.3_linux_amd64.zip
 cp packer /usr/local/bin/packer
 
+# Install Vagrant
+apt-get install -y vagrant
+
 # Build DARKSURGEON Build Path
 mkdir /opt/DARKSURGEON
 cd /opt/DARKSURGEON
